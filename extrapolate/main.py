80d2249261df40ef0173609d5e91cd8da703fcb2
kqf <q.dev.null@gmail.com>
2020-11-16 08:34:24 +0100

    Add params abstraction (#2)
    * Add params object
    
    * Move the param creation logic to the separate class methods
    
    * Add custom formatting
    
    * Run multiple analyses

--- extrapolate/main.py
+++ extrapolate/main.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 from iminuit.cost import LeastSquares
 from extrapolate.vis import vis
-from extrapolate.data import dataset, params
+from extrapolate.data import dataset
+from extrapolate.params import Params
 
 
 def ds(t, a1, b1, a2, b2, p1, b3, p2, c3):
@@ -17,31 +18,35 @@ def fit(data, pars, func):
     loss = LeastSquares(data["-t"], data["obs"], data["total err."], ds)
     minimizer = iminuit.Minuit(loss, pedantic=False, **pars)
     minimizer.migrad(ncall=10000)
-    minimizer.hesse(ncall=10000)
-    minimizer.minos(ncall=10000)
 
-    print()
-    print("energy at t\\neq 0", data["s"].min(), data["s"].max())
-    print("|t|", data["-t"].min(), data["-t"].max())
-    print("chi^2_tot ", minimizer.fval)
-    print("chi^2/dof ", minimizer.fval / (len(data) - minimizer.nfit))
+    # Print the short summary
+    print("energy at t\\neq 0 :", data["s"].min(), data["s"].max())
+    print("|t|               :", data["-t"].min(), data["-t"].max())
+    print("chi^2_tot         :", minimizer.fval)
+    print("chi^2/dof         :", minimizer.fval / (len(data) - minimizer.nfit))
+
+    pars_fitted = Params.from_minuit(minimizer.params)
+    print(pars_fitted)
+    print(pars_fitted.df)
     return minimizer
 
 
 def main():
     data = dataset((62.400, 62.600))
     data = data[data["-t"].between(0.5, 2.5)]
-    pars = params()
-    print(pars)
-    pars = pars.dropna()["value"].to_dict()
+    pars = Params.from_dat()
 
-    m = fit(data, pars, ds)
-    print(m.params)
+    print("Fit with the same limits")
+    m_limits = fit(data, pars.to_minuit(), ds)
+
+    print("Fit with no limits")
+    m_no_limits = fit(data, pars.values, ds)
 
     with vis(data, label="pp 62.5 Amaldi"):
         t = data["-t"]
-        plt.plot(t, ds(t, **pars), label="fit 1")
-        plt.plot(t, ds(t, **m.values), label="new")
+        plt.plot(t, ds(t, **pars.values), label="initial values")
+        plt.plot(t, ds(t, **m_limits.values), label="fit (same limits)")
+        plt.plot(t, ds(t, **m_no_limits.values), label="fit (no limits)")
         plt.legend()
 
 
---
+++ extrapolate/params.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+from contextlib import contextmanager
+from operator import attrgetter
+
+
+@contextmanager
+def custom_format(custom):
+    standard = pd.get_option('display.float_format')
+    pd.set_option('display.float_format', custom)
+    yield
+    pd.set_option('display.float_format', standard)
+
+
+def scientific(x, pattern="{:.5E}"):
+    return pattern.format(x)
+
+
+class Params:
+    fields = ["number", "name", "value", "error", "lower_limit", "upper_limit"]
+
+    def __init__(self, df):
+        self.df = df.set_index("name")
+
+    @property
+    def limits(self):
+        limits = self.df[["lower_limit", "upper_limit"]].copy()
+        limits.index = "limit_" + limits.index
+        limits["limit"] = limits.apply(tuple, axis=1)
+        return limits["limit"].to_dict()
+
+    @property
+    def values(self):
+        return self.df["value"].to_dict()
+
+    def to_minuit(self, limits=True):
+        output = {}
+        output.update(self.values)
+        if limits:
+            output.update(self.limits)
+        return output
+
+    @classmethod
+    def from_minuit(cls, params):
+        df = pd.DataFrame({"raw": params})
+        for field in cls.fields:
+            df[field] = df["raw"].apply(attrgetter(field))
+
+        # Fortran indexing
+        return cls(df.drop(columns=["raw"]))
+
+    @classmethod
+    def from_dat(cls, filename="data/params.dat", dropna=True):
+        df = pd.read_table(filename, sep=r"\s+", names=cls.fields)
+        if dropna:
+            df = df.dropna()
+        df["name"] = df["name"].str.replace("'", "")
+        return cls(df)
+
+    def __repr__(self):
+        with custom_format(scientific):
+            return str(self.df)
