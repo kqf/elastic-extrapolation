5fa3ae75361ade30e8bb059fedb9791574ef36ef
Sasha <q.dev.null@gmail.com>
2020-11-19 17:03:30 +0100

    Simplify the plotting methods

--- extrapolate/vis.py
+++ extrapolate/vis.py
@@ -3,14 +3,20 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def vis(data, func=None, pars=None, label=None):
+def vis(data, func=None, label=None):
     plt.errorbar(data["-t"], data["obs"],
                  yerr=data["total err."], fmt='.', markersize=0.1, label=label)
     if func is not None:
-        plt.plot(data["-t"], func(data["-t"], **pars))
+        plt.plot(data["-t"], func(data["-t"]), label="fit")
 
     plt.yscale("log")
     plt.xlabel("$|t|$ (GeV$^{2}$)")
     plt.ylabel(r"$d\sigma/dt$ (mb/GeV$^2$)")
-    yield
+    yield plt
     plt.show()
+
+
+def plot(data, func=None, label=None):
+    with vis(data, func, label) as plt:
+        plt.legend()
+        plt.savefig(f"{label}.png")
