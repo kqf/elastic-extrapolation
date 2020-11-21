ec6832f05de19d2fab1651256f850199be7fa17e
Sasha <q.dev.null@gmail.com>
2020-11-21 07:29:21 +0100

    Add the separate configuration for the error bars

--- extrapolate/vis.py
+++ extrapolate/vis.py
@@ -4,22 +4,33 @@ from contextlib import contextmanager
 
 @contextmanager
 def vis(data, func, label=None):
+    # Configure plotting
     a1 = plt.subplot2grid((4, 2), (0, 0), rowspan=3)
     a2 = plt.subplot2grid((4, 2), (3, 0), sharex=a1)
     a4 = plt.subplot2grid((4, 2), (0, 1), rowspan=4)
+    errorbar_cfg = dict(
+        ls='',
+        marker='o',
+        markersize=2,
+        # markerfacecolor='none',
+        capsize=1,
+        capthick=1,
+        # ecolor='black'
+    )
 
     t, y, dy = data["-t"], data["obs"], data["total err."]
     yhat = func(t)
 
     # Plot the data and the fitted function
-    a1.errorbar(t, y, yerr=dy, fmt='.', markersize=0.1, label=label)
+    a1.errorbar(t, y, yerr=dy, label=label, **errorbar_cfg)
+
     a1.plot(t, yhat, label="fit")
     a1.set_ylabel(r"$d\sigma/dt$ (mb/GeV$^2$)")
     a1.set_yscale('log')
     a1.legend()
 
     # Plot the ratio
-    a2.errorbar(t, y / yhat, yerr=dy / yhat, fmt='.')
+    a2.errorbar(t, y / yhat, yerr=dy / yhat, **errorbar_cfg)
     a2.plot(t, t * 0. + 1., 'k--')
     a2.set_xlabel("$|t|$ (GeV$^{2}$)")
     a2.set_ylabel("$\\frac{\\rm{data}}{\\rm{fit}}$")
