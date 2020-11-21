8719e49f360ab2e00da87c2877861fdb9b99bc70
Sasha <q.dev.null@gmail.com>
2020-11-21 07:13:54 +0100

    Add the error bars for the ratio plot

--- extrapolate/vis.py
+++ extrapolate/vis.py
@@ -8,22 +8,24 @@ def vis(data, func, label=None):
     a2 = plt.subplot2grid((4, 2), (3, 0), sharex=a1)
     a4 = plt.subplot2grid((4, 2), (0, 1), rowspan=4)
 
+    t, y, dy = data["-t"], data["obs"], data["total err."]
+    yhat = func(t)
+
     # Plot the data and the fitted function
-    a1.errorbar(data["-t"], data["obs"],
-                yerr=data["total err."], fmt='.', markersize=0.1, label=label)
-    a1.plot(data["-t"], func(data["-t"]), label="fit")
+    a1.errorbar(t, y, yerr=dy, fmt='.', markersize=0.1, label=label)
+    a1.plot(t, yhat, label="fit")
     a1.set_ylabel(r"$d\sigma/dt$ (mb/GeV$^2$)")
     a1.set_yscale('log')
     a1.legend()
 
     # Plot the ratio
-    a2.plot(data["-t"], data["obs"] / func(data["-t"]), '.')
-    a2.plot(data["-t"], data["-t"] * 0. + 1., 'k--')
+    a2.errorbar(t, y / yhat, yerr=dy / yhat, fmt='.')
+    a2.plot(t, t * 0. + 1., 'k--')
     a2.set_xlabel("$|t|$ (GeV$^{2}$)")
     a2.set_ylabel("$\\frac{\\rm{data}}{\\rm{fit}}$")
 
     # Plot the real to imaginary ratio
-    a4.errorbar(data["-t"], func(data["-t"]), label="fit")
+    a4.errorbar(t, func(t), label="fit")
     a4.set_ylabel(r"$\rho(t)$")
     a4.set_xlabel("$|t|$ (GeV$^{2}$)")
 
